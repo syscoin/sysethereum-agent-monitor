638872aef3b48b050e014569818ecce5ccc69889
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2020-04-28 17:51:21 -0400

    fix path

--- start_agent.sh
+++ start_agent.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd "home/ubuntu/sysethereum-agents"
+cd "/home/ubuntu/sysethereum-agents"
 java -Dsysethereum.agents.conf.file=/home/ubuntu/sysethereum-agents/data/sysethereum-agents.conf -Dhttps.protocols=TLSv1.2,TLSv1.1,TLSv1 -jar target/sysethereum-agents-1.0-jar-with-dependencies.jar
