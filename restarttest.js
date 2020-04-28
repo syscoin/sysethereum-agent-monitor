78aab2538d0787b19b04c7d1c3ebcce0ba09e073
dwasyluk <987186+dwasyluk@users.noreply.github.com>
2020-04-28 15:43:20 -0400

    fix process test

--- restarttest.js
+++ restarttest.js
@@ -4,7 +4,6 @@ run = async () => {
   try {
     console.log('Stopping all screens.');
     const pkill = spawnSync('screen', ['-list']);
-    console.log('out: ', pkill);
     Object.values(pkill.output).forEach(item => {
       if (!item) return;
       console.log("ITEMS:", item ? item.toString('utf8') : null);
