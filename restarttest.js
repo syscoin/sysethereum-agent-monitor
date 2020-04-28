ad3e56f8d25983a952d0396e5de34ba9c3faf9ab
Dan Wasyluk <987186+dwasyluk@users.noreply.github.com>
2020-04-28 17:34:30 -0400

    scripting

--- restarttest.js
+++ restarttest.js
@@ -1,9 +1,16 @@
 const { execSync } = require('child_process');
 
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+};
+
 run = async () => {
   try {
     console.log('Stopping syscoind');
     const syskill = execSync('syscoin-cli stop');
+
+    // wait for sys to shut down
+    await sleep(3000);
 
     console.log('Stopping screens.');
     const syseth = execSync('screen -XS agenttest quit');
@@ -17,9 +24,9 @@ run = async () => {
     const sysethup = execSync('screen -S sysethagent ./start_agent.sh');
 
   } catch (e) {
-    console.log('Error: ', e);
-    console.log('Stderr: ', e.stderr);
-    console.log('Stdout: ', e.stdout);
+    //sconsole.log('Error: ', e);
+    //console.log('Stderr: ', e.stderr);
+    //console.log('Stdout: ', e.stdout);
 
     return e;
   }
