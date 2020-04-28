ab2b3826264b7627e0d912f49800fb86ad954650
dwasyluk <987186+dwasyluk@users.noreply.github.com>
2020-04-28 15:04:40 -0400

    fix process test

--- restarttest.js
+++ restarttest.js
@@ -1,11 +1,26 @@
 const exec = require('exec-sh').promise;
 
 run = async () => {
-  // switch to screen with monitor
-  const pkill = await exec('pkill screen');
-  const sysstop = await exec('syscoin-cli stop');
-  console.log(pkill, sysstop);
-}
+  try {
+    console.log('Stopping all screens.');
+    const pkill = await exec('pkill screen');
+    console.log('out: ', pkill.stdout, pkill.stderr);
+
+
+    console.log('Stopping syscoind');
+    const sysstop = await exec('syscoin-cli stop');
+    console.log('out: ', sysstop.stdout, sysstop.stderr);
+
+    console.log("Results:");
+    console.log(pkill, sysstop);
+  } catch (e) {
+    console.log('Error: ', e);
+    console.log('Stderr: ', e.stderr);
+    console.log('Stdout: ', e.stdout);
+
+    return e;
+  }
+};
 
 run();
 
