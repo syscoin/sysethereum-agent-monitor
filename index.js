366ce62efe802bc3b20bc381e0e185cca99e81cb
dwasyluk <987186+dwasyluk@users.noreply.github.com>
2020-05-08 21:15:36 -0400

    debug

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function checkForAlerts(mailer, skipMail) {
     console.log('Attempting restart!!!');
     let reason;
     if (processStatus.isError) {
-      reason = 'One or more key processes (agent, syscoind, sysgeth, sysrelayer) has stopped unexpectedly.';
+      reason = 'One or more key processes (agent, syscoind, sysgeth, sysrelayer) has stopped unexpectedly. \\n';
       Object.keys(processStatus).forEach(key => {
         if (key !== 'isError') {
           // reason += `${key}: ${processStatus[key]} \n`;
