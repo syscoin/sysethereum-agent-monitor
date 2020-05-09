cbd693d9cca443fef8b3174f85a14d2165531aa2
dwasyluk <987186+dwasyluk@users.noreply.github.com>
2020-05-08 21:13:27 -0400

    debug

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function checkForAlerts(mailer, skipMail) {
       reason = 'One or more key processes (agent, syscoind, sysgeth, sysrelayer) has stopped unexpectedly.\n'
       Object.keys(processStatus).forEach(key => {
         if (key !== 'isError') {
-          reason += `${key}: ${processStatus[key]} \n`;
+          // reason += `${key}: ${processStatus[key]} \n`;
         }
       })
     } else if (sysStatus.isError) {
@@ -72,7 +72,7 @@ async function checkForAlerts(mailer, skipMail) {
       reason = 'Cannot determine!';
     }
     const tokenObj = {
-      reason: reason.toString()
+      reason
     };
 
     await utils.sendMail(mailer, require('./messages/agent_restart_in_progress'), tokenObj, true);
