516c9980852cbfd1d271bf917aacf80014678452
dwasyluk <987186+dwasyluk@users.noreply.github.com>
2020-05-08 21:20:12 -0400

    updates to restart message

--- index.js
+++ index.js
@@ -56,14 +56,17 @@ async function checkForAlerts(mailer, skipMail) {
     clearInterval(checkInterval);
     isAttemptingRestart = true;
     console.log('Attempting restart!!!');
-    let reason;
+    let reason = {};
     if (processStatus.isError) {
-      reason = 'One or more key processes (agent, syscoind, sysgeth, sysrelayer) has stopped unexpectedly. \\n';
+      reason.text = 'One or more key processes (agent, syscoind, sysgeth, sysrelayer) has stopped unexpectedly. \\n';
+      reason.html = 'One or more key processes (agent, syscoind, sysgeth, sysrelayer) has stopped unexpectedly. <br /><ul>';
       Object.keys(processStatus).forEach(key => {
         if (key !== 'isError') {
-          // reason += `${key}: ${processStatus[key]} \n`;
+          reason.text += `${key}: ${processStatus[key]} \\n`;
+          reason.html += `<li ${processStatus[key] === false ? `style='color:red; font-weight: bold` : ''$}>${key}: ${processStatus[key]} </li>`;
         }
       });
+      reason.html += '</ul>';
     } else if (sysStatus.isError) {
       reason = 'Syscoin full node is on wrong chain.';
     } else if (ethStatus.isError) {
--- messages/agent_restart_in_progress.js
+++ messages/agent_restart_in_progress.js
@@ -2,8 +2,8 @@ const config = require(`../config`);
 
 module.exports = {
   subject: `${config.agent_id} SYSETH INFO: One or more agent errors`,
-  text: `THE AGENT IS ATTEMPTING TO BE AUTO RESTARTED. NO ACTION REQUIRED YET.\\n Restart reason: {{reason}}.\nYou will receive another email with the restart status shortly.`,
+  text: `THE AGENT IS ATTEMPTING TO BE AUTO RESTARTED. NO ACTION REQUIRED YET.\\n Restart reason: {{reason.text}}.\nYou will receive another email with the restart status shortly.`,
   html: `<p><b>THE AGENT IS ATTEMPTING TO BE AUTO RESTARTED. NO ACTION REQUIRED YET.</b></p>
-         <p>Restart reason: {{reason}}</p>
+         <p>Restart reason: {{reason.html}}</p>
          <p>You will receive another email with the restart status shortly.</p>`
 };
